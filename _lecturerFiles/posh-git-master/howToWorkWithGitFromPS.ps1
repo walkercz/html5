831f576b8cbfa13f57b80e5583012cede78f7221
walkercz <lastovka.cz@gmail.com>
2017-02-16 14:57:08 +0100

    02/16/2017 14:57:08

---
+++ _lecturerFiles/posh-git-master/howToWorkWithGitFromPS.ps1
@@ -0,0 +1,13 @@
+﻿git init
+git add .
+git commit -m "first commit"
+git remote add origin https://github.com/walkercz/html5.git
+git config --global user.email "lastovka.cz@gmail.com"
+git config --global user.name "walkercz"
+
+git add .
+git commit -m "$(Get-Date)"
+git push origin master
+
+git config --global --unset-all
+# DO NOT FORGET TO DELETE STORED CREDENTIALS FROM Control Panel\User Accounts and Family Safety\Credential Manager
